e6461b21cc565afe1f3176b1d7622e3eac470d13
Dandara Salaun <43423357+dands-salaun@users.noreply.github.com>
2020-10-05 15:38:42 -0300

    Fix Sample

---
+++ Samples~/Example/Scripts/AutoDestroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Dands.Pool.Demo
+{
+    public class AutoDestroy : MonoBehaviour
+    {
+        private float timeDestroy = 5f;
+
+        private void OnEnable()
+        {
+            StartCoroutine(DelayDestroy());
+        }
+
+        private IEnumerator DelayDestroy()
+        {
+            yield return new WaitForSeconds(timeDestroy);
+            DPool.I.ReturnObject(gameObject);
+        }
+
+        private void OnDisable()
+        {
+            StopAllCoroutines();
+        }
+    }
+}
---
+++ Samples~/Example/Scripts/DemoDPool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Dands.Pool.Demo
+{
+    public class DemoDPool : MonoBehaviour
+    {
+        public float delayCreateNewObject = 1f;
+        public Transform positionCreateNewObject;
+        public string[] tagOfObjectsInThePool;
+
+        private void Start()
+        {
+            StartCoroutine(SpawnObject());
+        }
+
+        private IEnumerator SpawnObject()
+        {
+            yield return new WaitForSeconds(delayCreateNewObject);
+
+            GameObject newObject =
+                DPool.I.GetObject(RaffleObject(), positionCreateNewObject.position, Quaternion.identity);
+
+            StartCoroutine(SpawnObject());
+
+        }
+
+        private string RaffleObject()
+        {
+            string raffleTag = "";
+
+            if (tagOfObjectsInThePool.Length > 0)
+            {
+                raffleTag = tagOfObjectsInThePool[Random.Range(0, tagOfObjectsInThePool.Length)];
+            }
+
+            return raffleTag;
+        }
+    }
+}
